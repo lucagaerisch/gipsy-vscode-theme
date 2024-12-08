fe1a0ff3ba8cd054b917a46279ee1aa2cbf60933
Luca Gärisch <gaerisch.luca@gmail.com>
2024-12-08 23:33:20 +0100

    some changes
    fixed formatting
    added basics for a light theme
    removed unnecessary files

--- src/core/theme-builder.ts
+++ src/core/theme-builder.ts
@@ -5,14 +5,18 @@ import { VsTheme } from '../types';
 const themesDir = path.resolve(__dirname, '..', '..', 'themes');
 
 export async function themeBuilder(theme: VsTheme) {
+  try {
     if (!fs.existsSync(themesDir)) {
-        await fs.promises.mkdir(themesDir, { recursive: true });
+      await fs.promises.mkdir(themesDir, { recursive: true });
     }
 
-    const themeData = JSON.stringify(theme); 
+    const themeData = JSON.stringify(theme);
     const filePath = path.join(themesDir, `${theme.id}.json`);
 
     await fs.promises.writeFile(filePath, themeData);
 
     console.log(`Theme "${theme.name}" saved at: ${filePath}`);
+  } catch (error) {
+    console.log(`Error while saving Theme: [${theme.id}], Error: [${error}]`);
+  }
 }
--- src/gipsy.ts
+++ src/gipsy.ts
@@ -1,11 +1,20 @@
-import { themeBuilder } from "./core/theme-builder";
-import { generateTheme } from "./core/theme-generator";
+import { themeBuilder } from './core/theme-builder';
+import { generateTheme } from './core/theme-generator';
 
 async function run() {
-
+  try {
     const gipsyDark = generateTheme('gipsy-dark', 'Gipsy Dark', 'dark');
+    const gipsyLight = generateTheme('gipsy-light', 'Gipsy Light', 'ligth');
 
+    // Build all themes
+    // Themes also need to be added in the package.json
     await themeBuilder(gipsyDark);
+    await themeBuilder(gipsyLight);
+  } catch (error) {
+    console.log(
+      `Some error occured while generating the themes! Error:[${error}]`,
+    );
+  }
 }
 
 run();
